9fc9314ab5c3840b9f1377b8507c74d23b0fd05d
toly <tolytrader@gmail.com>
2014-04-25 00:00:09 +0400

    save only significant keys of nodes

--- app/scripts/resources/trees.js
+++ app/scripts/resources/trees.js
@@ -11,8 +11,31 @@ angular.module('gtdTreeApp')
             return this.trees[project_id] || [{name: "Node", nodes: [], root: true}];
         }
 
+        function filter_significant_keys(nodes) {
+            var significant_keys = [
+                'id',
+                'name',
+                'done',
+                'parent'
+            ];
+
+            var result_nodes = $.map(nodes, function(obj){
+                return $.extend({}, obj);
+            });
+
+            for (var i = 0; i < result_nodes.length; i++) {
+                for (var key in result_nodes[i]) {
+                    if ( significant_keys.indexOf(key) < 0 ) {
+                        delete result_nodes[i][key];
+                    }
+                }
+            }
+
+            return result_nodes;
+        }
+
         function save_project_tree(project_id, tree_data){
-            this.trees[project_id] = tree_data;
+            this.trees[project_id] = filter_significant_keys(tree_data);
             localStorageService.add(trees_key, this.trees);
         }
 
